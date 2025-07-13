7e6702055356203f9b1acc5f209f4966658c119c
macbook <macbook@MacBook-Air.local>
2025-07-13 16:19:55 +0100

    Update package.json

--- server.js
+++ server.js
@@ -27,8 +27,11 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
-  },
+    origin: [
+      "http://localhost:3000",
+      "https://bytelearn-online-school.onrender.com"
+    ]
+  }
 });
 
 io.use((socket, next) => {
